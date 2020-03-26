9c8c4b678ddc7030752aaf94e4ab199001451e6a
Dmitriy Efremov <me@arren.ru>
2020-03-26 22:12:05 +0700

    Fixed spec

--- Casks/starsector.rb
+++ Casks/starsector.rb
@@ -1,12 +1,12 @@
 cask 'starsector' do
-  version '0.9.1a-RC8'
-  sha256 "b11e2d15c5ce16572ac8731f2ab649183a3f285b39ea6c8dc74ece7b8dc9fc78"
-
-  url 'http://www.fractalsoftworks.com/starsector/release/starsector_mac-#{version}.zip'
-  name 'Starsector'
-  homepage 'https://fractalsoftworks.com/'
+  name "Starsector"
+  version "0.9.1a-RC8"
+  homepage "https://fractalsoftworks.com/"
 
   auto_updates true
-  app '#{name}.app'
+
+  url "http://www.fractalsoftworks.com/starsector/release/starsector_mac-#{version}.zip"
+  sha256 "b11e2d15c5ce16572ac8731f2ab649183a3f285b39ea6c8dc74ece7b8dc9fc78"
+  app "#{name}.app"
 end
 
