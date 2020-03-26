da704abc8e4b22084ebf6301ea369641e52a7901
Dmitriy Efremov <me@arren.ru>
2020-03-26 22:31:52 +0700

    Disable auto_updates for starsector

--- Casks/starsector.rb
+++ Casks/starsector.rb
@@ -3,8 +3,6 @@ cask 'starsector' do
   version "0.9.1a-RC8"
   homepage "https://fractalsoftworks.com/"
 
-  auto_updates true
-
   url "http://www.fractalsoftworks.com/starsector/release/starsector_mac-#{version}.zip"
   sha256 "b11e2d15c5ce16572ac8731f2ab649183a3f285b39ea6c8dc74ece7b8dc9fc78"
   app "Starsector.app"
