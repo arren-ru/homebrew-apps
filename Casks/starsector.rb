5e0385c24705c49c4e0b7f3a5119add763cdfbec
Dmitriy Efremov <me@arren.ru>
2020-03-26 22:15:27 +0700

    Fixed spec

--- Casks/starsector.rb
+++ Casks/starsector.rb
@@ -7,6 +7,6 @@ cask 'starsector' do
 
   url "http://www.fractalsoftworks.com/starsector/release/starsector_mac-#{version}.zip"
   sha256 "b11e2d15c5ce16572ac8731f2ab649183a3f285b39ea6c8dc74ece7b8dc9fc78"
-  app "#{name}.app"
+  app "Starsector.app"
 end
 
