43fcad3c1b5c2a8798ecf93e9f1978476882e0a5
Dmitriy Efremov <me@arren.ru>
2020-04-08 13:51:20 +0700

    Added reconquest/orgalorg

---
+++ orgalorg.rb
@@ -0,0 +1,28 @@
+class Orgalorg < Formula
+  desc "Parallel SSH commands executioner and file synchronization tool"
+  version "200115.158_70019e7"
+  url "https://github.com/reconquest/orgalorg.git", :revision => "70019e77521102efe3d062f31e2573ef10993334"
+
+  bottle :unneeded
+
+  depends_on "go" => :build
+  def install
+    ENV["GOPATH"] = buildpath
+    
+    contents = buildpath.children - [buildpath/".brew_home"]
+    (buildpath/"src/github.com/reconquest/orgalorg").install contents
+    (buildpath/"bin").mkpath
+
+    cd "src/github.com/reconquest/orgalorg" do
+      system "go", "get"
+      system "go", "build", "-x", "-ldflags=-X main.version=#{version}", "-gcflags=-trimpath #{buildpath}/src"
+      bin.install "orgalorg"
+    end
+  end
+
+#  test do
+#    system "#{bin}/zabbixctl", "--version"
+#    assert_match "#{version}", shell_output("#{bin}/zabbixctl --version")
+#  end
+end
+
