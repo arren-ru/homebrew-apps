3082246d1922fd1097fdb7d7c7d02d549a63729d
Dmitriy Efremov <me@arren.ru>
2020-11-24 16:06:20 +0700

    Moved to arren-ru/homebrew-starsector

--- starsector.rb
+++
@@ -1,9 +0,0 @@
-cask 'starsector' do
-  name "Starsector"
-  version "0.9.1a-RC8"
-  homepage "https://fractalsoftworks.com/"
-
-  url "http://www.fractalsoftworks.com/starsector/release/starsector_mac-#{version}.zip"
-  sha256 "b11e2d15c5ce16572ac8731f2ab649183a3f285b39ea6c8dc74ece7b8dc9fc78"
-  app "Starsector.app"
-end
